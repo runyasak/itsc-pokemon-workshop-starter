d8a1a931db112a93d1ef1bcc3a88900eda645549
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-11-03 16:12:16 +0700

    feat: remove unused components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+import HomeView from "./views/HomeView.vue";
+</script>
+
 <template>
   <div class="navbar bg-sky-500 flex justify-center">
     <div class="text-2xl text-white">Pokedex</div>
   </div>
   <div class="p-4 lg:p-8">
-    <RouterView />
+    <HomeView></HomeView>
   </div>
 </template>
